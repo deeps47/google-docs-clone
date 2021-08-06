48ea7a68e02c84b4baf83b583bbaa309bfd243f8
Conqueror14M <dy6579@srmist.edu.in>
2021-08-06 22:16:44 +0530

    ADD: async function

--- pages/doc/[id].js
+++ pages/doc/[id].js
@@ -59,3 +59,13 @@ function Doc() {
 }
 
 export default Doc
+
+export async function getServerSideProps(context) {
+    const session = await getSession(context);
+
+    return {
+        props: {
+            session
+        }
+    }
+}
