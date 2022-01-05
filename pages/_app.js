6ae3c1a7b8ff9cb63b2cda7dc2730f0ab67540c5
Deepanshu Yadav <dy6579@srmist.edu.in>
2022-01-05 16:58:35 +0530

    Fix: responsiveness

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
-
+import "../styles.css"
 import 'tailwindcss/tailwind.css'
 import "@material-tailwind/react/tailwind.css"
 import Head from "next/head"
 import { Provider } from "next-auth/client"
-import "../styles.css"
+
 
 function MyApp({ Component, pageProps }) {
   return (
